be76a3d14fd2f1c0d797107e77a74a5242487cd4
MattPonton <Matt.Ponton@gmail.com>
2026-01-24 16:52:05 -0500

    Ignore vertex calls when not drawing. Increases performance of DOA2U Rain and Burai Zenin.

--- hw/xbox/nv2a/pgraph/vk/vertex.c
+++ hw/xbox/nv2a/pgraph/vk/vertex.c
@@ -54,7 +54,7 @@ void pgraph_vk_update_vertex_ram_buffer(PGRAPHState *pg, hwaddr offset,
     size_t nbits = end_bit - start_bit;
 
     if (find_next_bit(r->uploaded_bitmap, start_bit + nbits, start_bit) <
-        end_bit) {
+        end_bit && r->in_draw) {
         // Vertex data changed while building the draw list. Finish drawing
         // before updating RAM buffer.
         pgraph_vk_finish(pg, VK_FINISH_REASON_VERTEX_BUFFER_DIRTY);
